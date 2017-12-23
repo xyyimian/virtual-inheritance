40b21da6f09bec90f50bb2d48e29914a398da18c
yu-xing <xyyimian@mail.ustc.edu.cn>
2017-12-23 18:20:46 +0800

    multiple_base_classes added

---
+++ test/mumltiple_base_classes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class A{
+public:
+	int ii;
+};
+
+class B{
+public:
+	char* ii;
+};
+
+class C:public A, public B{
+
+};
+
+int main(){
+	
+	C* pc = 0;
+	B* pb = 0;
+	if(pb == 0){
+		cout << "pb is assigned to zero" << endl;
+	}
+	pb = pc;		//pb = (pc == 0)?0:(B*)((char*)pc + delta(B));
+	if(pb == 0){
+		cout << "pb is casted to zero" << endl;		//can be normally operated
+	}
+	
+
+}
